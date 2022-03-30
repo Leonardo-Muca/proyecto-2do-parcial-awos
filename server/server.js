3071fa1f866edb2e58233fca8b82c9583eab61ca
Leonardo Munoz <leonardo.munoz@easypayfinance.com>
2022-03-30 11:45:56 -0600

    test

--- server/server.js
+++ server/server.js
@@ -19,7 +19,7 @@ app.use((req, res, next) => {
 app.use(bodyParser.json());
 app.use(cors());
 app.get('/', function (req, res) {
-  res.send('<h1>Bienvenido amigo (localhost)</h1>');
+  res.send(require('../index.html'));
 });
 
 app.use('/api',require('./routes/index'));
